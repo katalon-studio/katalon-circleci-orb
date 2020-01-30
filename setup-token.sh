7c5d6ecb757f0c40886a0755d241f843dd68ad6f
Dat Quach <datquach@kms-technology.com>
2020-01-30 14:30:15 +0700

    update

--- setup-token.sh
+++ setup-token.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 mkdir ~/.circleci/
-echo "host: https://circleci.com" >  ~/.circleci/cli.yml
-echo "endpoint: graphql-unstable" >> ~/.circleci/cli.yml
 cat > ~/.circleci/cli.yml <<- "EOF"
 host: https://circleci.com
 endpoint: graphql-unstable
