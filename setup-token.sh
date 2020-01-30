dae04e7370b464bc4565c33c9e178aa6251f5dc6
Dat Quach <datquach@kms-technology.com>
2020-01-30 14:49:44 +0700

    update

--- setup-token.sh
+++ setup-token.sh
@@ -3,5 +3,5 @@ mkdir ~/.circleci/
 cat > ~/.circleci/cli.yml <<- "EOF"
 host: https://circleci.com
 endpoint: graphql-unstable
-token: $CIRCLECITOKEN
 EOF
+echo "token: $CIRCLECITOKEN" >> ~/.circleci/cli.yml
